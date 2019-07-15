80a762936fd7924cf2f04a903130448d0a148b68
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-15 15:56:38 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+const body = document.getElementById("body");
+
+let index = 0;
+
+body.addEventListener('keydown', init());
+
+
+function init(){
+  const key = event.key;
+  
+  if (key === codes[index]){
+    index ++;
+  }
+  
+  if (index === alphabet.length) {
+    alert('You got dis!');
+  }
 }
